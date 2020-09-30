ee2fd2558c5a199de92975527f54b2ad37d36ae1
Robert Schloen <rschloen@gmail.com>
2020-09-30 13:13:53 -0500

    Moved Hexagons into separate component
    To make it easier to use the hexagons in different pages, I extracted
    the hexagon creation code and made a new Hexagon component that can be
    imported into other parent components like Manual.
    
    Added event dispatchers to Hexagon component that call functions in
    parent component (ei: Manual) like buildCommandBlock for normal use and
    testing.
    
    Used bind:prop to communicate changes to props, like activeHexagon and
    test_ui, in the parent component to Hexagon component.

--- src/routes/Automatic.svelte
+++ src/routes/Automatic.svelte
@@ -1,7 +1,11 @@
 <script>
     import {Router} from 'svelte-routing';
+    import Hexagons from '../Utils/Hexagons.svelte';
+
+    export let test_ui = false;
 </script>
 
 <Router>
     <h1>Automatic</h1>
+    <Hexagons bind:test_ui/>
 </Router>
