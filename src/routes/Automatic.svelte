e358a53967c4eb5108cd29f2f6c45c1dbea42bc5
Robert Schloen <rschloen@gmail.com>
2020-10-14 21:24:12 -0500

    Added images to replace buttons for options
    Instead of a button, the background of the div container in the option
    link window is set to an image, such as the location of actuators on the
    body.

--- src/routes/Automatic.svelte
+++ src/routes/Automatic.svelte
@@ -1,20 +1,22 @@
 <script>
 import { Router, Link, Route } from 'svelte-routing';
-
-
 import Devices from '../Utils/Devices.svelte';
 import Hexagons from '../Utils/Hexagons.svelte';
+
+$: img1 = 'images/preset_chest.png';
+
 
 </script>
 
 <Router>
-    <h1>Automatic</h1>
-    <Devices />
-
+    <div class="col-25">
+        <h1>Automatic</h1>
+        <Devices />
+    </div>
     <div class='scroll-box'>
         <div class='option'>
             <!-- Option 1 is default -->
-            <Link to="/"><button class="button">Option 1</button></Link>
+            <Link to="/"><div class="button" style="background-image:url({img1});"></div></Link>
         </div>
         <div class='option' >
             <Link to="OP2"><button class="button">Option 2</button></Link>
@@ -23,12 +25,12 @@ import Hexagons from '../Utils/Hexagons.svelte';
             <Link to="OP3"><button class="button">Option 3</button></Link>
         </div>
     </div>
+
     <Route path="/">
         <Hexagons/>
     </Route>
-
     <Route path="OP2">
-        <Hexagons/>
+        <Hexagons orientation="vertical"/>
     </Route>
     <Route path="OP3">
         <Hexagons/>
@@ -39,10 +41,10 @@ import Hexagons from '../Utils/Hexagons.svelte';
 <style>
     .scroll-box{
         width: 15%;
-        height: 500px;
+        height: 50em;
 
         padding: 1em;
-        margin: 10px;
+        margin: 3em;
 
         display: inline-block;
         vertical-align: top;
@@ -50,24 +52,35 @@ import Hexagons from '../Utils/Hexagons.svelte';
 
         outline: solid;
         outline-color: black;
-        outline-width: 5px;
+        outline-width: .4em;
     }
 
     .option{
         width: 100%;
-        height: 100px;
-        margin-top: 10px;
-        margin-bottom: 10px;
+        height: 30%;
+        margin-top: 1em;
+        margin-bottom: 1em;
         outline: solid;
         outline-color: black;
-        outline-width: 3px;
+        outline-width: .3em;
     }
 
     .button{
         width: 100%;
         height: 100%;
-        background-color: white;
+        background-size: 100% 100%;
+        background-position: center; 
+        background-repeat: no-repeat;
     }
     
+    .col-25 {
+        width: 15%;
+        height: 100%;
 
+        display: inline-block;
+        vertical-align: top;
+
+        margin: 0px auto;
+        padding: 1em;
+    } 
 </style>
