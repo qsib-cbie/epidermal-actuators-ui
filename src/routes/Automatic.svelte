7e868090681c62b4be0d6caf4fef156d6b9f8694
Robert Schloen <rschloen@gmail.com>
2020-11-30 14:47:23 -0600

    Added presets to virtual touch page
    A preset is turned on by pressing its toggleable button on the right.
    Blue border means its on and a click or swipe on the actuators will
    trigger it.

--- src/routes/Automatic.svelte
+++ src/routes/Automatic.svelte
@@ -8,6 +8,10 @@ let hexActive;
 let hexType;
 let is_active = 0;
 
+let presetName;
+let usePreset = false;
+let lastPreset;
+
 $: main_img = back_img;
 let back_img = "images/back.png";
 let chest_img = "images/chest.png";
@@ -16,13 +20,26 @@ let shoulder_img = "images/shoulder.png";
 let thigh_img = "images/thigh.png";
 let upper_arm_img = "images/upper_arm.png";
 
+function setPreset(name) {
+    presetName = name;
+    let element = document.getElementById(name);
+    if (lastPreset) {lastPreset.style = "";}
+    if (lastPreset == element) {
+        usePreset = false;
+        lastPreset = null;
+    } else {
+        usePreset = true;
+        element.style =  "border: .2em solid blue;";
+        lastPreset = element;
+    }
+}
 </script>
 
 <div>
     <div class='scroll-box'>
         <div class='option'>
             <!-- Option 0 is default -->
-                <button class="button" style={"background-image:url("+back_img+");"} on:click={() => {main_img = back_img; is_active=0;}}>
+                <button class="op-button" style={"background-image:url("+back_img+");"} on:click={() => {main_img = back_img; is_active=0; usePreset=false; lastPreset.style = "";}}>
                     {#if (is_active == 0)}
                         <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
                     {:else}
@@ -31,7 +48,7 @@ let upper_arm_img = "images/upper_arm.png";
                 </button>
         </div>
         <div class='option' >
-                <button class="button" style={"background-image:url("+upper_arm_img+");"} on:click={() => {main_img = upper_arm_img; is_active=1;}}>
+                <button class="op-button" style={"background-image:url("+upper_arm_img+");"} on:click={() => {main_img = upper_arm_img; is_active=1; usePreset=false; lastPreset.style = "";}}>
                     {#if (is_active == 1)}
                         <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
                     {:else}
@@ -40,7 +57,7 @@ let upper_arm_img = "images/upper_arm.png";
                 </button>
         </div>
         <div class='option' >
-                <button class="button" style={"background-image:url("+chest_img+");"} on:click={() => {main_img = chest_img; is_active=2;}}>
+                <button class="op-button" style={"background-image:url("+chest_img+");"} on:click={() => {main_img = chest_img; is_active=2; usePreset=false; lastPreset.style = "";}}>
                     {#if (is_active == 2)}
                         <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
                     {:else}
@@ -49,7 +66,7 @@ let upper_arm_img = "images/upper_arm.png";
                 </button>
         </div>
         <div class='option' >
-                <button class="button" style={"background-image:url("+thigh_img+");"} on:click={() => {main_img = thigh_img; is_active=3;}}>
+                <button class="op-button" style={"background-image:url("+thigh_img+");"} on:click={() => {main_img = thigh_img; is_active=3; usePreset=false; lastPreset.style = "";}}>
                    {#if (is_active == 3)}
                         <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
                     {:else}
@@ -58,7 +75,7 @@ let upper_arm_img = "images/upper_arm.png";
                 </button>
         </div>
         <div class='option' >
-                <button class="button" style={"background-image:url("+hand_img+");"} on:click={() => {main_img = hand_img; is_active=4;}}>
+                <button class="op-button" style={"background-image:url("+hand_img+");"} on:click={() => {main_img = hand_img; is_active=4; usePreset=false; lastPreset.style = "";}}>
                     {#if (is_active == 4)}
                         <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
                     {:else}
@@ -67,7 +84,7 @@ let upper_arm_img = "images/upper_arm.png";
                 </button>
         </div>
         <div class='option' >
-                <button class="button" style={"background-image:url("+shoulder_img+");"} on:click={() => {main_img = shoulder_img; is_active=5;}}> 
+                <button class="op-button" style={"background-image:url("+shoulder_img+");"} on:click={() => {main_img = shoulder_img; is_active=5; usePreset=false; lastPreset.style = "";}}> 
                     {#if (is_active == 5)}
                         <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
                     {:else}
@@ -78,8 +95,26 @@ let upper_arm_img = "images/upper_arm.png";
     </div>
 
     <div class="virtual-touch">
-        <div class="button" style={"background-image:url("+main_img+"); background-size: 200% 200%;"}>
-            <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType}/>
+        <div class="op-button" style={"background-image:url("+main_img+"); background-size: 200% 200%;"}>
+            <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} presetName={presetName} isPreset={usePreset}/>
+        </div>
+    </div>
+
+    <div class="scroll-box">
+        <div class="option">
+            <button id="FlashAll" class="op-button" on:click={() => setPreset("FlashAll")}>
+                Flash All
+            </button>
+        </div>
+        <div class="option">
+            <button id="sweep" class="op-button" on:click={() => setPreset("sweep")}>
+                Sweep
+            </button>
+        </div>
+        <div class="option">
+            <button id="ABCs" class="op-button" on:click={() => setPreset("ABCs")}>
+                ABCs
+            </button>
         </div>
     </div>
 </div>
@@ -95,35 +130,22 @@ let upper_arm_img = "images/upper_arm.png";
         display: inline-block;
         vertical-align: top;
         overflow: auto;
-
-        outline: none;
-        /* outline-color: black;
-        outline-width: .2em; */
     }
 
     .option{
         width: 100%;
         height: 30%;
-        /* margin-top: 1em;
-        margin-bottom: 1em; */
-        outline: none;
-        /* outline-color: black;
-        outline-width: .3em; */
     }
 
-    .button{
+    .op-button{
         width: 100%;
         height: 100%;
         background-color: white;
         background-size: 100% 100%;
         background-position: center; 
         background-repeat: no-repeat;
-        color: white;
+        color: black;
     }
-    /* .button:focus{
-        background-color: rgb(0, 183, 255);
-    } */
-     
     .virtual-touch{
         width: 75%;
         height: 100%;
