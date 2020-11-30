4e97c7b86332325234f4b40906b0f5ef8cbfefa0
Robert Schloen <rschloen@gmail.com>
2020-11-30 12:20:02 -0600

    Refactored link between option and main container
    Added images for the different body location options.
    
    Made minor changes to style of scrollbox and option buttons
    
    Switched from using Router to link options and main hexagon to using if
    statements and click events. This reduces amount of code since each link
     doesn't need to be specified.
    
    Made one central container for main hexagon array that is conditionally
    bound to hexagons on the last button pressed. That means binding is only
    active for one button at a time.

--- src/routes/Automatic.svelte
+++ src/routes/Automatic.svelte
@@ -4,119 +4,134 @@ import Devices from '../Utils/Devices.svelte';
 import Hexagons from '../Utils/Hexagons.svelte';
 import { message } from "../../stores/stores.js";
 
-let numArray = 1;
-let array = [{id:0, name:"original"}];
+let hexActive;
+let hexType;
+let is_active = 0;
 
-$: img1 = "images/preset_chest.png";
-
-function handleAddActuators() { 
-    array[numArray] = {id: numArray, name: "array "+numArray.toString(), type: "stich"};
-    numArray++;
-}
+$: main_img = back_img;
+let back_img = "images/back.png";
+let chest_img = "images/chest.png";
+let hand_img = "images/hand.png";
+let shoulder_img = "images/shoulder.png";
+let thigh_img = "images/thigh.png";
+let upper_arm_img = "images/upper_arm.png";
 
 </script>
 
-<Router>
+<div>
     <div class='scroll-box'>
         <div class='option'>
-            <!-- Option 1 is default <div class="button" style="background-image:url({img1});"></div>  -->
-            <Link to="/"><button style="width: 100%;
-        height: 100%;
-        background-image:url({img1});
-        background-color: rgb(0, 119, 255);
-        background-size: 100% 100%;
-        background-position: center; 
-        background-repeat: no-repeat;
-        color: white;"><Hexagons arraySize="small" isPreset= true/></button></Link>
+            <!-- Option 0 is default -->
+                <button class="button" style={"background-image:url("+back_img+");"} on:click={() => {main_img = back_img; is_active=0;}}>
+                    {#if (is_active == 0)}
+                        <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
+                    {:else}
+                        <Hexagons arraySize="small" isPreset= true/>
+                    {/if}
+                </button>
         </div>
         <div class='option' >
-            <Link to="OP2"><button class="button">Option 2 </button></Link> 
-            <!-- <Hexagons arraySize="small" orientation="vertical"/> -->
+                <button class="button" style={"background-image:url("+upper_arm_img+");"} on:click={() => {main_img = upper_arm_img; is_active=1;}}>
+                    {#if (is_active == 1)}
+                        <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
+                    {:else}
+                        <Hexagons arraySize="small" isPreset= true/>
+                    {/if}
+                </button>
         </div>
         <div class='option' >
-            <Link to="OP3"><button class="button">Option 3</button></Link>
+                <button class="button" style={"background-image:url("+chest_img+");"} on:click={() => {main_img = chest_img; is_active=2;}}>
+                    {#if (is_active == 2)}
+                        <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
+                    {:else}
+                        <Hexagons arraySize="small" isPreset= true/>
+                    {/if}
+                </button>
+        </div>
+        <div class='option' >
+                <button class="button" style={"background-image:url("+thigh_img+");"} on:click={() => {main_img = thigh_img; is_active=3;}}>
+                   {#if (is_active == 3)}
+                        <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
+                    {:else}
+                        <Hexagons arraySize="small" isPreset= true/>
+                    {/if}
+                </button>
+        </div>
+        <div class='option' >
+                <button class="button" style={"background-image:url("+hand_img+");"} on:click={() => {main_img = hand_img; is_active=4;}}>
+                    {#if (is_active == 4)}
+                        <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
+                    {:else}
+                        <Hexagons arraySize="small" isPreset= true/>
+                    {/if}
+                </button>
+        </div>
+        <div class='option' >
+                <button class="button" style={"background-image:url("+shoulder_img+");"} on:click={() => {main_img = shoulder_img; is_active=5;}}> 
+                    {#if (is_active == 5)}
+                        <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType} arraySize="small" isPreset= true/>
+                    {:else}
+                        <Hexagons arraySize="small" isPreset= true/>
+                    {/if}
+                </button>
         </div>
     </div>
 
-    <Route path="/">
-        <!-- backgroundAsset={img1} -->
-        <div class="col-50">
-            <Hexagons />
+    <div class="virtual-touch">
+        <div class="button" style={"background-image:url("+main_img+"); background-size: 200% 200%;"}>
+            <Hexagons bind:activeHexagon={hexActive} bind:arrayType={hexType}/>
         </div>
-    </Route>
-    <Route path="OP2">
-        <div class="col-50">
-            <Hexagons orientation="vertical"/>
-        </div>
-        
-    </Route>
-    <Route path="OP3">
-        <div class="col-50">
-            <Hexagons />
-        </div>
-    </Route>
-</Router>
-
+    </div>
+</div>
 
 <style>
     .scroll-box{
-        width: 15%;
-        height: 50em;
+        width: 10%;
+        height: 100%;
 
-        padding: 1em;
-        margin: 3em;
+        padding: 0em;
+        margin: 0em;
 
         display: inline-block;
         vertical-align: top;
         overflow: auto;
 
-        outline: solid;
-        outline-color: black;
-        outline-width: .4em;
+        outline: none;
+        /* outline-color: black;
+        outline-width: .2em; */
     }
 
     .option{
         width: 100%;
         height: 30%;
-        margin-top: 1em;
-        margin-bottom: 1em;
-        outline: solid;
-        outline-color: black;
-        outline-width: .3em;
+        /* margin-top: 1em;
+        margin-bottom: 1em; */
+        outline: none;
+        /* outline-color: black;
+        outline-width: .3em; */
     }
 
     .button{
         width: 100%;
         height: 100%;
-        background-image:url({img1});
-        background-color: rgb(0, 119, 255);
+        background-color: white;
         background-size: 100% 100%;
         background-position: center; 
         background-repeat: no-repeat;
         color: white;
     }
-    .button:focus{
+    /* .button:focus{
         background-color: rgb(0, 183, 255);
-    }
-    
-    .col-25 {
-        width: 15%;
-        height: 100%;
-
-        display: inline-block;
-        vertical-align: top;
-
-        margin: 0px auto;
-        padding: 1em;
-    } 
-    .col-50 {
-        width: 50%;
+    } */
+     
+    .virtual-touch{
+        width: 75%;
         height: 100%;
 
         display: inline-block;
         vertical-align: middle;
-
-        margin: 0em auto;
+        text-align: center;
+        margin: 0em;
         padding: 1em;
     }
 </style>
